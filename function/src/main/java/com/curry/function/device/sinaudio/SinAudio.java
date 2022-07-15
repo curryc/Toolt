cf77aa08667e6dba0801ba629e87024f989998a8
curryc <chencurry5@gmail.com>
2022-07-15 23:56:00 +0800

    first function

---
+++ function/src/main/java/com/curry/function/device/sinaudio/SinAudio.java
@@ -0,0 +1,12 @@
+package com.curry.function.device.sinaudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @program: Toolt
+ * @author: 陈博文
+ * @create: 2022-07-15 23:01
+ * @description: 发生正弦波
+ **/
+public class SinAudio extends AppCompatActivity {
+}
