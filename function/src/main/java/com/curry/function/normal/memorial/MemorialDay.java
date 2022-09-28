424b232c86317e1b2e408870e7770cc78fb5cc97
curryc <chencurry5@gmail.com>
2022-09-28 22:10:12 +0800

    memorial day

--- function/src/main/java/com/curry/function/normal/memorial/MemorialDay.java
+++ function/src/main/java/com/curry/function/normal/memorial/MemorialDay.java
@@ -1,26 +1,65 @@
 package com.curry.function.normal.memorial;
 
-import com.curry.function.base.BaseBackActivity;
+import java.util.Date;
 
 /**
  * @program: Toolt
  * @author: 陈博文
- * @create: 2022-09-25 19:12
- * @description: 纪念日
+ * @create: 2022-09-28 15:01
+ * @description: 描述一个纪念日的bean
  **/
-public class MemorialDay extends BaseBackActivity {
-    @Override
-    protected int LayoutId() {
-        return 0;
+public class MemorialDay {
+    private Date mDate;
+    private int mImportance;
+    private String mDescription;
+    private boolean mRepeatEveryYear;
+    private String mName;
+
+    public MemorialDay(Date date, String name, int importance, String description, boolean repeatEveryYear) {
+        mDate = date;
+        mName = name;
+        mImportance = importance;
+        mDescription = description;
+        mRepeatEveryYear = repeatEveryYear;
     }
 
-    @Override
-    protected void initView() {
-
+    public Date getDate() {
+        return mDate;
     }
 
-    @Override
-    protected String getWindowTitle() {
-        return null;
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public int getImportance() {
+        return mImportance;
+    }
+
+    public void setImportance(int importance) {
+        mImportance = importance;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public boolean isRepeatEveryYear() {
+        return mRepeatEveryYear;
+    }
+
+    public void setRepeatEveryYear(boolean repeatEveryYear) {
+        mRepeatEveryYear = repeatEveryYear;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
     }
 }
